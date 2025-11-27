43513a8708a81d0a7e0a0209f21a6edebd70811e
scobosb <s.cobosb.2022@alumnos.urjc.es>
2025-11-27 20:45:18 +0100

    Added elbow and shoulder setup methods to BrutusLegController class

--- src/brutus_esp32/BrutusLegController.h
+++ src/brutus_esp32/BrutusLegController.h
@@ -9,6 +9,42 @@
 class BrutusLegController
 {
 
+private:
+  Pca9685Servo elbow;
+  Pca9685Servo shoulder;
+
+  float elbow_offset;
+  float shoulder_offset;
+
+public:
+  BrutusLegController()
+  {
+  }
+
+  void setup_elbow(Adafruit_PWMServoDriver* pca,
+                   int pca_idx,
+                   int min_pwm_pulse_period,
+                   int max_pwm_pulse_period,
+                   float min_angle,
+                   float max_angle.
+                   float angle_offset)
+  {
+    elbow = Pca9685Servo(pca, pca_idx, min_pwm_pulse_period, max_pwm_pulse_period, min_angle, max_angle);
+    elbow_offset = angle_offset;
+  }
+
+  void setup_shoulder(Adafruit_PWMServoDriver* pca,
+                      int pca_idx,
+                      int min_pwm_pulse_period,
+                      int max_pwm_pulse_period,
+                      float min_angle,
+                      float max_angle.
+                      float angle_offset)
+  {
+    shoulder = Pca9685Servo(pca, pca_idx, min_pwm_pulse_period, max_pwm_pulse_period, min_angle, max_angle);
+    shoulder_offset = angle_offset;
+  }
+
 }
 
 #endif // BRUTUS_LEG_CONTROLLER__H
--- src/brutus_esp32/brutus_esp32.ino
+++ src/brutus_esp32/brutus_esp32.ino
@@ -2,6 +2,11 @@
 
 Adafruit_PWMServoDriver pca = Adafruit_PWMServoDriver();
 
+BrutusLegController fr_leg = BrutusLegController();
+BrutusLegController fl_leg = BrutusLegController();
+BrutusLegController br_leg = BrutusLegController();
+BrutusLegController bl_leg = BrutusLegController();
+
 void setup() {
   init_brutus(&pca);
 }
