9c12dd85bdfabd29f6eeeba742dc7fcfecc43be3
Sergio Sanchez <s.sanchezh.2022@alumnos.urjc.es>
2025-12-01 19:10:29 +0100

    MQTT_ESP version 1.0

--- src/mqtt_test/mqtt_test.ino
+++ src/mqtt_test/mqtt_test.ino
@@ -1,51 +1,190 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "credential.h"
 
-// ----------- WIFI CONFIGURATION -----------
-#define SSID = "TU_WIFI";
-#define PASSWORD = "TU_PASSWORD";
+#define CLIENT_ID "brutus_esp"
 
-// ----------- MQTT CONFIGURATION  -----------
-#define MQTT_SERVER "test.mosquitto.org
-#define MQTT_PORT 1885
+// ----------- TOPICS SUBS-----------
+
+#define TOPIC_STATE_MODE "brutus/state/mode"
+#define TOPIC_CMD_MODE   "brutus/cmd/mode"
+#define TOPIC_CMD_VEL   "brutus/cmd/velocity"
+#define TOPIC_CMD_POSE   "brutus/cmd/pose"
+
+// ----------- TOPICS PUBS-----------
+
+#define TOPIC_STATE_HEARTBEAT "brutus/data/heartbeat"
+#define TOPIC_POSE            "brutus/data/pose"
+#define TOPIC_DIST_FRONT      "brutus/data/distance/front"
+#define TOPIC_DIST_RIGHT      "brutus/data/distance/right"
+#define TOPIC_DIST_LEFT       "brutus/data/distance/left"
+
+#define STATE_HEARTBEAT 0
+#define POSE            1
+#define DIST_FRONT      2
+#define DIST_RIGHT      3
+#define DIST_LEFT       4
+
+// ----------- OTHER -----------
+
+#define MSG_BUFFER 100
+#define PUB_FREQ 1000
+#define BAUD 115200
+#define WIFI_WAIT 500
+#define RECONNECT_WAIT 3000
+#define SHOULDER 0
+#define ELBOW 1
+
+// CREATE MSG
+void create_msg(int topic, const void* val, char* msg);
+
+// SUBS
+void callback(char* topic, byte* payload, unsigned int length);
+void handleStateMode(const char* msg);
+void handleCmdMode(const char* msg);
+void handleCmdVel(const char* msg);
+void handleCmdPose(const char* msg);
+
+
+void reconnect();
+
+struct TopicHandlerSub {
+  const char* topic;
+  void (*handler)(const char* msg);
+};
+
+struct TopicHandlerPub {
+  int n;
+  const char* topic;
+};
+
+
+struct leg {
+  int ang_shoulder;
+  int ang_elbow;
+};
+
+struct legs {
+  leg fr;
+  leg fl;
+  leg br;
+  leg bl;
+};
+
+
+TopicHandlerSub topicHandlers_sub[] = {
+  { TOPIC_CMD_POSE, handleCmdPose },
+  { TOPIC_STATE_MODE, handleStateMode },
+  { TOPIC_CMD_MODE, handleCmdMode },
+  { TOPIC_CMD_VEL, handleCmdVel }
+};
+
+const int numTopics = sizeof(topicHandlers_sub) / sizeof(topicHandlers_sub[0]);
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// ----------- Función que se ejecuta cuando llega un mensaje -----------
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensaje recibido en ");
-  Serial.print(topic);
-  Serial.print(": ");
+  char message[MSG_BUFFER];
 
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+  if (length >= sizeof(message)) {
+    Serial.print("Message received too long: ");
+    return;
   }
 
-  Serial.println();
+  memcpy(message, payload, length);
+  message[length] = '\0';
+
+  for (int i = 0; i < numTopics; i++) {
+    if (strcmp(topic, topicHandlers_sub[i].topic) == 0) {
+      topicHandlers_sub[i].handler(message);
+      return;
+    }
+  }
+
+  Serial.print("Message received on unknown topic: ");
+  Serial.println(topic);
 }
 
-// ----------- Conexión MQTT -----------
+    
+// ----------- HANDLERS -----------
+void handleCmdPose(const char* msg) {
+  Serial.print("handleCmdPose called with message: ");
+  Serial.println(msg);
+}
+
+void handleStateMode(const char* msg) {
+  Serial.print("handleStateMode called with message: ");
+  Serial.println(msg);
+
+}
+
+void handleCmdVel(const char* msg) {
+  Serial.print("handleCmdVel called with message: ");
+  Serial.println(msg);
+}
+
+void handleCmdMode(const char* msg) {
+  Serial.print("handleCmdMode called with message: ");
+  Serial.println(msg);
+}
+
+void create_msg(int topic, const void* val, char* msg, int msg_size) {
+  switch(topic) {
+    case STATE_HEARTBEAT:
+      snprintf(msg, msg_size, "{\"timestamp\":%lu}", millis());
+      break;
+
+    case POSE: {
+      const legs* l = static_cast<const legs*>(val);
+
+      snprintf(msg, msg_size, "{\"fr\":{%d:%d,%d:%d}, \"fl\":{%d:%d,%d:%d}, \"br\":{%d:%d,%d:%d}, \"bl\":{%d:%d,%d:%d}}",
+                                  SHOULDER, l->fr.ang_shoulder, ELBOW, l->fr.ang_elbow,
+                                  SHOULDER, l->fl.ang_shoulder, ELBOW, l->fl.ang_elbow,
+                                  SHOULDER, l->br.ang_shoulder, ELBOW, l->br.ang_elbow,
+                                  SHOULDER, l->bl.ang_shoulder, ELBOW, l->bl.ang_elbow);
+      break;
+      
+    }
+    case DIST_FRONT:
+      snprintf(msg, msg_size, "{\"front_dist\":%d}", *(int*)val);
+      break;
+
+    case DIST_RIGHT:
+      snprintf(msg, msg_size, "{\"right_dist\":%d}", *(int*)val);
+      break;
+
+    case DIST_LEFT:
+      snprintf(msg, msg_size, "{\"left_dist\":%d}", *(int*)val);
+      break;
+  }
+}
+
 void reconnect() {
   while (!client.connected()) {
-    Serial.print("Intentando conectar al broker MQTT...");
-    if (client.connect("ESP32_mqtt_test")) {
-      Serial.println("Conectado!");
+    Serial.print("Attempting to connect to the MQTT broker...");
 
-      // Suscribirse a un tema
-      client.subscribe("esp32/ejemplo/entrada");
+    if (client.connect(CLIENT_ID)) {
+      Serial.println("Connected!");
+
+      client.subscribe(TOPIC_CMD_POSE);
+      client.subscribe(TOPIC_CMD_VEL);
+      client.subscribe(TOPIC_STATE_MODE);
+      client.subscribe(TOPIC_CMD_MODE);
+
     } else {
-      Serial.print("Fallo. Código: ");
+      Serial.print("Failed. Code: ");
       Serial.print(client.state());
-      Serial.println(" Intentando de nuevo en 3 segundos...");
-      delay(3000);
+      Serial.println(" Trying again in 3 seconds...");
+      delay(RECONNECT_WAIT);
     }
   }
 }
 
+
 // ----------- SETUP -----------
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(BAUD);
 
   // Wifi conection
   Serial.print("Conectando a ");
@@ -53,30 +192,70 @@ void setup() {
   WiFi.begin(SSID, PASSWORD);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(WIFI_WAIT);
     Serial.print(".");
   }
 
   Serial.println("\nWiFi connected!");
 
-  // Configuración del cliente MQTT
   client.setServer(MQTT_SERVER, MQTT_PORT);
   client.setCallback(callback);
 }
 
+
 // ----------- LOOP PRINCIPAL -----------
+unsigned long last_msg = 0;
+
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
-
   client.loop();
 
-  // Publicar un mensaje cada 5 segundos
-  static unsigned long lastMsg = 0;
-  if (millis() - lastMsg > 5000) {
-    lastMsg = millis();
-    client.publish("esp32/ejemplo/salida", "Hola desde ESP32!");
-    Serial.println("Mensaje publicado!");
+  unsigned long now = millis();
+  if (now - last_msg > PUB_FREQ) {
+
+    char msg[MSG_BUFFER];
+    create_msg(STATE_HEARTBEAT, 0, msg, MSG_BUFFER);
+
+    if (!client.publish(TOPIC_STATE_HEARTBEAT, msg)) {
+      Serial.println("Error publicando STATE_HEARTBEAT");
+    }
+
+    legs l = {
+      {random(0, 181), random(0, 181)}, // fr
+      {random(0, 181), random(0, 181)}, // fl
+      {random(0, 181), random(0, 181)}, // br
+      {random(0, 181), random(0, 181)}  // bl
+    };
+    create_msg(POSE, &l, msg, MSG_BUFFER);
+
+    if (!client.publish(TOPIC_POSE, msg)) {
+      Serial.println("Error publicando POSE");
+    }
+
+    int front_dist = random(0, 100);
+    int right_dist = random(0, 100);
+    int left_dist = random(0, 100);
+    create_msg(DIST_FRONT, &front_dist, msg, MSG_BUFFER);
+
+    if (!client.publish(TOPIC_DIST_FRONT, msg)) {
+      Serial.println("Error publicando DIST_FRONT");
+    }
+
+    create_msg(DIST_RIGHT, &right_dist, msg, MSG_BUFFER);
+
+    if (!client.publish(TOPIC_DIST_RIGHT, msg)) {
+      Serial.println("Error publicando DIST_RIGHT");
+    }
+
+    create_msg(DIST_LEFT, &left_dist, msg, MSG_BUFFER);
+
+    if (!client.publish(TOPIC_DIST_LEFT, msg)) {
+      Serial.println("Error publicando DIST_LEFT");
+    }
+
+    last_msg = now;
   }
 }
+
